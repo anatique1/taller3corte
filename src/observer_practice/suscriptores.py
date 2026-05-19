07ac7d0a949fbf21f12a17103672a77d07615857
anatique1 <ana.tique1@estudiantesunibague.edu.co>
2026-05-19 17:33:57 -0500

    ana

--- src/observer_practice/suscriptores.py
+++ src/observer_practice/suscriptores.py
@@ -10,9 +10,3 @@ class SuscriptorEmail:
     def __str__(self):
         return f"{self.nombre} por {self.canal}"
     
-    from observer_practice.suscriptores import SuscriptorEmail
-
-ana = SuscriptorEmail("Ana")
-ana.actualizar("Primera noticia")
-print(ana.mensajes)
-print(ana)
