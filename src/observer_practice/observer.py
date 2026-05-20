9331d2b3ee5be63631a6e1df84cd4c8c020aa95f
anatique1 <ana.tique1@estudiantesunibague.edu.co>
2026-05-19 19:57:52 -0500

    observer

--- src/observer_practice/observer.py
+++ src/observer_practice/observer.py
@@ -4,4 +4,11 @@ from typing import Protocol
 class Observador(Protocol):
     def actualizar(self, mensaje):
         ...
+def notificar(self, mensaje):
+    for observador in self.observadores:
+        observador.actualizar(mensaje)
+
+def publicar(self, mensaje):
+    self.ultimo_mensaje = mensaje
+    self.notificar(mensaje)    
 
